8a12b34908cd83e5cb35a7d456b896fa206e2a70
weesteekygames <weesteekygames@outlook.com>
2020-03-01 15:33:00 +0000

    PROJECT: init commit

---
+++ MapGenSrc/Assets/Scripts/scr_terrain_generation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_terrain_generation : MonoBehaviour
+{
+    public GameObject obj_house;
+
+    private const int num_houses = 10;
+    private float width = 10.0f;
+    private float height = 3.0f; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // create houses 
+        for (int i = 0; i < num_houses; ++i) {
+            Vector3 position = new Vector3(Random.Range(-width,width), Random.Range(-height,height), 0);
+            Instantiate(obj_house, position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
---
+++ MapGenSrc/Assets/Scripts/scr_town_properties.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_town_properties : MonoBehaviour
+{
+    private string m_town_name;
+    private int m_population; 
+
+    // mentality
+    enum Menatily { PEACEFUL = 0, WARLIKE = 1 };
+    private int m_mentality_int;
+    private Menatily m_mental;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Random.Range(0, 100) < 50)
+        {
+            m_mentality_int = 1;
+        }
+        else
+        {
+            m_mentality_int = 0;
+        }
+
+        Debug.Log(m_mentality_int);
+        m_mental = (Menatily)m_mentality_int;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
